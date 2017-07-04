d234145de3d02c4c3767d4654dbe80b098a2c2a2
Muthu <m.kumar@icmarkets.com.au>
2017-07-04 12:32:31 +1000

    Format the output, 'var' to 'let'

--- win-bets.js
+++ win-bets.js
@@ -1,59 +1,59 @@
-// require('babel-register');
-    
 let CalcUtilities = require('./divident-calc-utilities.js')
 
 let HORSE_INDEX = 2;  // index in the input format
 let BET_AMOUNT_INDEX = 3;  // INdex in the input format
 
-export default class WinBetsModule{
-    
-    constructor(){
+export default class WinBetsModule {
+
+    constructor() {
         // console.log( "const win bet module")
         this.winBetsList = [];
-        this.calcUtilities  = new CalcUtilities();
+        this.calcUtilities = new CalcUtilities();
         this.winBetsResult = {};
     }
 
-    addBets(betStr){
-        var winInputArr = betStr.split(":");
-        // console.log( " Wind Bets : before adding",this.winBetsList);
+    addBets(betStr) {
+        let winInputArr = betStr.split(":");
         this.winBetsList.push({
             horseNumber: parseInt(winInputArr[HORSE_INDEX]),
-            betAmount:parseInt(winInputArr[BET_AMOUNT_INDEX])
+            betAmount: parseInt(winInputArr[BET_AMOUNT_INDEX])
         });
     }
-    getBetsList(){
+
+    getBetsList() {
         return this.getBetsList;
     }
-    calculateDivident(winningHorse){
+
+    calculateDivident(winningHorse) {
         // this.winBetsList = this.getTestBets();  // uncomment for unit testing
         this.winBetsResult.winnerHorse = winningHorse;
 
-        var totalPool = this.calcUtilities.totalBetAmount(this.winBetsList),
-                winnerTotalBetAmount = 0,
-                remaining,
-                commission;
-            
+        let totalPool = this.calcUtilities.totalBetAmount(this.winBetsList),
+            winnerTotalBetAmount = 0,
+            remaining,
+            commission;
+
         winnerTotalBetAmount = this.calcUtilities.totalBetAmountForhorseNumber(this.winBetsList, winningHorse);
         this.winBetsResult.totalPool = totalPool;
         this.winBetsResult.winnerTotalBetAmount = winnerTotalBetAmount;
         this.winBetsResult.commission = 15 * totalPool / 100; // 15%
         this.winBetsResult.remaining = totalPool - this.winBetsResult.commission;
-        if( this.winBetsResult.winnerTotalBetAmount){
+        if (this.winBetsResult.winnerTotalBetAmount) {
             this.winBetsResult.divident = Math.round(this.winBetsResult.remaining / winnerTotalBetAmount * 100) / 100;
-        }else{
-            this.winBetsResult.divident = 'No Win bet for horse number : ' + this.winBetsResult.winnerHorse ;
         }
-        // console.log('totalPool', totalPool, 'winnerTotalBetAmount', winnerTotalBetAmount);
-        // console.log('this.winBetsResult', JSON.stringify(this.winBetsResult));
     }
-    
-    getResults(){
-        return "Win:" + this.winBetsResult.winnerHorse +":$" + this.winBetsResult.divident;
+
+    getResults() {
+        if (!!this.winBetsResult.divident) {
+            return "Win:" + this.winBetsResult.winnerHorse + ":$" + this.winBetsResult.divident;
+        } else {
+            return "Win:" + this.winBetsResult.winnerHorse + ": No Win bet for this horse number";
+        }
     }
-    // internal for unit testing
-    getTestBets(){
-        return  [
+
+    // internal : for unit testing
+    getTestBets() {
+        return [
             {horseNumber: 1, betAmount: 3},
             {horseNumber: 2, betAmount: 4},
             {horseNumber: 3, betAmount: 5},
@@ -67,9 +67,7 @@ export default class WinBetsModule{
             {horseNumber: 3, betAmount: 63},
             {horseNumber: 4, betAmount: 15}
         ];
-        
-    }
 
     }
 
-// module.exports.WinBetsModule = WinBetsModule;
+}
