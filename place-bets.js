0eb19935099532a990860f5078cfc6ff5d7659fa
Muthu <m.kumar@icmarkets.com.au>
2017-07-04 12:28:39 +1000

    Code cleanup and handle corner cases

--- place-bets.js
+++ place-bets.js
@@ -1,42 +1,37 @@
-// require('babel-register');
-    
 let CalcUtilities = require('./divident-calc-utilities.js')
 
 let HORSE_INDEX = 2;  // index in the input format
 let BET_AMOUNT_INDEX = 3;  // INdex in the input format
 
-export default class PlaceBetsModule{
-    
-    constructor(){
+export default class PlaceBetsModule {
+
+    constructor() {
         // console.log( "const place bet module")
         this.placeBetsList = [];
-        this.calcUtilities  = new CalcUtilities();
+        this.calcUtilities = new CalcUtilities();
         this.placeBetsResult = {};
     }
 
-    addBets(betStr){
-        var placeInputArr = betStr.split(":");
+    addBets(betStr) {
+        let placeInputArr = betStr.split(":");
         // console.log( " Wind Bets : before adding",this.placeBetsList);
         this.placeBetsList.push({
             horseNumber: parseInt(placeInputArr[HORSE_INDEX]),
-            betAmount:parseInt(placeInputArr[BET_AMOUNT_INDEX])
+            betAmount: parseInt(placeInputArr[BET_AMOUNT_INDEX])
         });
     }
-    getBetsList(){
+
+    getBetsList() {
         return this.getBetsList;
     }
-    calculateDivident(winSeq){
-        this.placeBetsList = this.getTestBets();  // uncomment for unit testing
+
+    calculateDivident(winSeq) {
+        // this.placeBetsList = this.getTestBets();  // uncomment for unit testing
         this.placeBetsResult.winSeq = winSeq;
 
-        var totalPool = this.calcUtilities.totalBetAmount(this.placeBetsList),
-            winnerTotalBetAmount = 0,
-            remaining,
-            commission,
-            place1Div,
-            place2Div,
-            place3Div;
-    //1. Calculate total amount
+        let totalPool = this.calcUtilities.totalBetAmount(this.placeBetsList),
+            winnerTotalBetAmount = 0
+        //1. Calculate total amount
 
         this.placeBetsResult.totalPool = totalPool;
         this.placeBetsResult.commission = 12 * totalPool / 100; // 12%
@@ -44,35 +39,52 @@ export default class PlaceBetsModule{
         // this.placeBetsResult.totalDivident = Math.round(this.placeBetsResult.remaining / winnerBe * 100) / 100;
         this.placeBetsResult.seqDividents = [];
 
-        for (var i = 0; i < winSeq.length; i++) {
+        let divideBy = winSeq.length;  // initial group, assume all the group has valid bet placed
+        for (let i = 0; i < winSeq.length; i++) {
+            let totalForhorseNumber = this.calcUtilities.totalBetAmountForhorseNumber(this.placeBetsList, winSeq[i]);
+            if (totalForhorseNumber === 0) {
+                divideBy--;
+            }
             this.placeBetsResult.seqDividents.push({
                 horseNumber: winSeq[i],
-                divident: Math.round(100 * ( this.placeBetsResult.remaining / (this.calcUtilities.totalBetAmountForhorseNumber(this.placeBetsList, winSeq[i]) * 3))) / 100  // one third for each group divided by total
-            })
+                totalPlaceBetAmount: totalForhorseNumber
+            });
+
         }
-            // console.log('totalPool', totalPool, 'winnerTotalBetAmount', winnerTotalBetAmount);
-            // console.log('this.placeBetsResult', JSON.stringify(this.placeBetsResult));
+        for (let j = 0; j < winSeq.length; j++) {
+            if (this.placeBetsResult.seqDividents[j].totalPlaceBetAmount) {
+                // divideby  for each group divided by total
+                this.placeBetsResult.seqDividents[j].divident = Math.round(100 * ( this.placeBetsResult.remaining /
+                        (this.placeBetsResult.seqDividents[j].totalPlaceBetAmount * divideBy))) / 100;
+            } else {
+                // no place bet plaaced for this horse
+                this.placeBetsResult.seqDividents[j].divident = 0;
+            }
+        }
     }
-    
-    getResults(){
+
+    getResults() {
         // return "Win:" + this.placeBetsResult.winnerHorse +":$" + this.placeBetsResult.divident;
-         var result="";
-         for( var i=0; i< this.placeBetsResult.seqDividents.length; i++){
-                if( this.placeBetsResult.seqDividents[i].divident){
-                    result +=  "Place:" + this.placeBetsResult.seqDividents[i].horseNumber +
-                             ":$" + this.placeBetsResult.seqDividents[i].divident +"\n"
-                }else{
-                     result +=  "Place:" + this.placeBetsResult.seqDividents[i].horseNumber +
-                        ": No bet placed for this horse \n"
-                }
-        
+        let result = "";
+        for (let i = 0; i < this.placeBetsResult.seqDividents.length; i++) {
+            if (this.placeBetsResult.seqDividents[i].divident) {
+                result += "Place:" + this.placeBetsResult.seqDividents[i].horseNumber +
+                    ":$" + this.placeBetsResult.seqDividents[i].divident
+            } else {
+                result += "Place:" + this.placeBetsResult.seqDividents[i].horseNumber +
+                    ": No bet placed for this horse "
+            }
+            if (i !== this.placeBetsResult.seqDividents.length - 1) {
+                result += "\n";  // Add new line except last line
+            }
         }
         return result;
     }
+
     // internal for unit testing
-    getTestBets(){
-        return  [
-           {horseNumber: 1, betAmount: 31},
+    getTestBets() {
+        return [
+            {horseNumber: 1, betAmount: 31},
             {horseNumber: 2, betAmount: 89},
             {horseNumber: 3, betAmount: 28},
             {horseNumber: 4, betAmount: 72},
@@ -85,9 +97,6 @@ export default class PlaceBetsModule{
             {horseNumber: 3, betAmount: 39},
             {horseNumber: 4, betAmount: 105}
         ];
-        
-    }
 
     }
-
-// module.exports.WinBetsModule = WinBetsModule;
+}
